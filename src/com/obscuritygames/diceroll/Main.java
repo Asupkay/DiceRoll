28cd3f44178c7ec25e1ccb93a232d4b3fe5c1269
Alex Supkay <asupkay1124@gmail.com>
2017-07-15 04:08:52 -0400

    Add message for argument not being an int

--- src/com/obscuritygames/diceroll/Main.java
+++ src/com/obscuritygames/diceroll/Main.java
@@ -21,9 +21,14 @@ public class Main extends JavaPlugin {
 			Command command,
 			String label,
 			String[] args) {
-		if(command.getName().equalsIgnoreCase("roll") && args.length == 1 && isInt(args[0])) {
-			sender.sendMessage("You ran /roll with number of sides " + args[0] + " number of args: " + args.length + " Is int: " + isInt(args[0]));
-			return true;
+		if(command.getName().equalsIgnoreCase("roll") && args.length == 1) {
+			if(isInt(args[0])) {
+				sender.sendMessage("You ran /roll with number of sides " + args[0] + " number of args: " + args.length + " Is int: " + isInt(args[0]));
+				return true;
+			} else {
+				sender.sendMessage("The argument of /roll [number of size] must be a number");
+				return true;
+			}
 		}
 		return false;
 	}
